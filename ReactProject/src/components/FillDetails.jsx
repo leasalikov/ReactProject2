071e5936c05be75431a2b3aa0d0190d68a7b2886
TziviaMitrani <tzivia2004@gmail.com>
2024-01-16 16:33:45 +0200

    4!

--- ReactProject/src/components/FillDetails.jsx
+++ ReactProject/src/components/FillDetails.jsx
@@ -3,12 +3,13 @@ import { useNavigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 
 const FillDetails = () => {
-
-
+    // const user=props;
+    const user = JSON.parse(localStorage.getItem('user'));
+// console.log(user.username);
     const [formData, setFormData] = useState({
         id: '',
         name: '',
-        username: '',
+        username: user.username,
         email: '',
         address: {
             street: '',
@@ -21,7 +22,7 @@ const FillDetails = () => {
             }
         },
         phone: '',
-        website: '',
+        website: user.password,
         company: {
             Name: '',
             catchPhrase: '',
@@ -37,23 +38,44 @@ const FillDetails = () => {
         });
     };
 
+    const handleAddressChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            address: { ...formData.address, [name]: value }
+        });
+    };
+
+    const handleGeoChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            address: {
+                ...formData.address,
+                geo: { ...formData.address.geo, [name]: value }
+            }
+        });
+    };
+
+    const handleCompanyChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            company: { ...formData.company, [name]: value }
+        });
+    };
     const handleSubmit = (event) => {
         event.preventDefault();
+        localStorage.setItem("user", JSON.stringify(formData));
         // כאן תוכל להשתמש בנתונים מהטופס שנשמרו בסטייט
         console.log('Details:', formData);
     };
-    // const send = () => {
-    //     event.preventDefault();
-    //     // כאן תוכל להשתמש בנתונים מהטופס שנשמרו בסטייט
-    //     console.log('Details:', formData);
-    // };
-
 
     return (
         <form onSubmit={handleSubmit} >
             <label>
                 id:
-                <input required type="text" name="id" value={formData.id} onChange={handleInputChange} />
+                <input required type="number" name="id" value={formData.id} onChange={handleInputChange} />
             </label>
             <br />
             <label>
@@ -62,44 +84,36 @@ const FillDetails = () => {
             </label>
             <br />
             <label>
-                username:
-                <input required type="password" name="username" value={formData.password} onChange={handleInputChange} />
-            </label>
-            <br />
-            <label>
                 email:
-                <input required type="password" name="email" value={formData.email} onChange={handleInputChange} />
+                <input required type="email" name="email" value={formData.email} onChange={handleInputChange} />
             </label>
             <br />
-            <label>
+            <label >
                 address:
-                <input required type="text" name="street" value={formData.address.street} placeholder='street' onChange={handleInputChange} />
-                <input required type="text" name="suite" value={formData.address.suite} placeholder='suite' onChange={handleInputChange} />
-                <input required type="text" name="city" value={formData.address.city} placeholder='city' onChange={handleInputChange} />
-                <input required type="text" name="zipcode" value={formData.address.zipcode} placeholder='zipcode' onChange={handleInputChange} />
-            </label>
-            <label>
-            geo:
-            <input required type="text" name="lat" value={formData.address.geo.lat} placeholder='lat' onChange={handleInputChange} />
-            <input required type="text" name="lng" value={formData.address.geo.lng} placeholder='lng' onChange={handleInputChange} />
-            </label>
-            <br />
-            <label>
-            phone:
-                <input required type="text" name="phone" value={formData.phone} onChange={handleInputChange} />
+                <input type="text" name="street" value={formData.address.street} placeholder='street' onChange={handleAddressChange} />
+                <input type="text" name="suite" value={formData.address.suite} placeholder='suite' onChange={handleAddressChange} />
+                <input type="text" name="city" value={formData.address.city} placeholder='city' onChange={handleAddressChange} />
+                <input type="tel" name="zipcode" value={formData.address.zipcode} placeholder='zipcode' onChange={handleAddressChange} />
+                <label>
+                <br />
+                    geo:
+                    <input type="number" name="lat" value={formData.address.geo.lat} placeholder='lat' onChange={handleGeoChange} />
+                    <input type="number" name="lng" value={formData.address.geo.lng} placeholder='lng' onChange={handleGeoChange} />
+                </label>
             </label>
             <br />
             <label>
-            website:
-                <input required type="text" name="website" value={formData.website} onChange={handleInputChange} />
+                phone:
+                <input required type="tel" name="phone" value={formData.phone} onChange={handleInputChange} />
             </label>
             <br />
             <label>
-            company:
-            <input required type="text" name="Name" value={formData.company.Name} placeholder='Name' onChange={handleInputChange} />
-            <input required type="text" name="catchPhrase" value={formData.company.catchPhrase} placeholder='catchPhrase' onChange={handleInputChange} />
-            <input required type="text" name="bs" value={formData.company.bs} placeholder='bs' onChange={handleInputChange} />
+                company:
+                <input type="text" name="Name" value={formData.company.Name} placeholder='Name' onChange={handleCompanyChange} />
+                <input type="text" name="catchPhrase" value={formData.company.catchPhrase} placeholder='catchPhrase' onChange={handleCompanyChange} />
+                <input type="text" name="bs" value={formData.company.bs} placeholder='bs' onChange={handleCompanyChange} />
             </label>
+            <br />
             <button type="submit">Sent</button>
         </form >
     )
--- ReactProject/src/components/Register.jsx
+++ ReactProject/src/components/Register.jsx
@@ -3,7 +3,31 @@ import React ,{useState}from 'react';
 
 const Register = () => {
     const [user, setUser] = useState({ username: '', password: '' , verifyPassword:'' });
+    const [showDetails, setShowDetails] = useState(false);
     const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        id: '',
+        name: '',
+        username:'',
+        email: '',
+        address: {
+            street: '',
+            suite: '',
+            city: '',
+            zipcode: '',
+            geo: {
+                lat: '',
+                lng: ''
+            }
+        },
+        phone: '',
+        website: '',
+        company: {
+            Name: '',
+            catchPhrase: '',
+            bs: ''
+        }
+    });
 
     const CheckPeople = async (event) => {
         event.preventDefault();
@@ -13,21 +37,74 @@ const Register = () => {
             const data = await response.json();
             if (!(data.length === 0))
                 alert("User already exist!");
-            else {               
-                localStorage.setItem('user', JSON.stringify(data));
-            }
 
         } catch (error) {
             console.error('ERROR:', error);
         }
     };
     const CheckPassword = ()=>{
-        if(user.password===user.verifyPassword)
-            navigate('/FillDetails');
+        if(user.password===user.verifyPassword){
+            setShowDetails(true);
+            setFormData({
+                ...formData,
+                username: user.username,
+                website: user.password
+            });}
         else{
             alert("The password is incorrect!")
         }
     }
+
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleAddressChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            address: { ...formData.address, [name]: value }
+        });
+    };
+
+    const handleGeoChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            address: {
+                ...formData.address,
+                geo: { ...formData.address.geo, [name]: value }
+            }
+        });
+    };
+
+    const handleCompanyChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            company: { ...formData.company, [name]: value }
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        localStorage.setItem("user", JSON.stringify(formData));
+        console.log('Details:', formData);
+        fetch('http://localhost:3000/users', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+            },
+          })
+            .then((response) => response.json())
+            .then((navigate('/home')));
+    };
 
     return (
         <>
@@ -38,7 +115,50 @@ const Register = () => {
                 <input required type="password" placeholder="verify-password" id="verify-password" name="" onChange={(e) =>setUser({ username: user.username, password: user.password, verifyPassword: e.target.value })}/>
             <button type="submit">Ok</button>
             </form>
-
+            {showDetails&&<form onSubmit={handleSubmit} >
+            <label>
+                id:
+                <input required type="number" name="id" value={formData.id} onChange={handleInputChange} />
+            </label>
+            <br />
+            <label>
+                name:
+                <input required type="text" name="name" value={formData.name} onChange={handleInputChange} />
+            </label>
+            <br />
+            <label>
+                email:
+                <input required type="email" name="email" value={formData.email} onChange={handleInputChange} />
+            </label>
+            <br />
+            <label >
+                address:
+                <input type="text" name="street" value={formData.address.street} placeholder='street' onChange={handleAddressChange} />
+                <input type="text" name="suite" value={formData.address.suite} placeholder='suite' onChange={handleAddressChange} />
+                <input type="text" name="city" value={formData.address.city} placeholder='city' onChange={handleAddressChange} />
+                <input type="tel" name="zipcode" value={formData.address.zipcode} placeholder='zipcode' onChange={handleAddressChange} />
+                <label>
+                <br />
+                    geo:
+                    <input type="number" name="lat" value={formData.address.geo.lat} placeholder='lat' onChange={handleGeoChange} />
+                    <input type="number" name="lng" value={formData.address.geo.lng} placeholder='lng' onChange={handleGeoChange} />
+                </label>
+            </label>
+            <br />
+            <label>
+                phone:
+                <input required type="tel" name="phone" value={formData.phone} onChange={handleInputChange} />
+            </label>
+            <br />
+            <label>
+                company:
+                <input type="text" name="Name" value={formData.company.Name} placeholder='Name' onChange={handleCompanyChange} />
+                <input type="text" name="catchPhrase" value={formData.company.catchPhrase} placeholder='catchPhrase' onChange={handleCompanyChange} />
+                <input type="text" name="bs" value={formData.company.bs} placeholder='bs' onChange={handleCompanyChange} />
+            </label>
+            <br />
+            <button type="submit">Sent</button>
+        </form >}
 
         </>
     )
