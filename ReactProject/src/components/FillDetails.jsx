f98c4975951ed4b50d00f16de0a01c3704581c89
TziviaMitrani <tzivia2004@gmail.com>
2024-01-16 12:57:06 +0200

    3.5

--- ReactProject/src/components/FillDetails.jsx
+++ ReactProject/src/components/FillDetails.jsx
@@ -50,55 +50,55 @@ const FillDetails = () => {
 
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} >
             <label>
                 id:
-                <input type="text" name="id" value={formData.id} onChange={handleInputChange} />
+                <input required type="text" name="id" value={formData.id} onChange={handleInputChange} />
             </label>
             <br />
             <label>
                 name:
-                <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
+                <input required type="text" name="name" value={formData.name} onChange={handleInputChange} />
             </label>
             <br />
             <label>
                 username:
-                <input type="password" name="username" value={formData.password} onChange={handleInputChange} />
+                <input required type="password" name="username" value={formData.password} onChange={handleInputChange} />
             </label>
             <br />
             <label>
                 email:
-                <input type="password" name="email" value={formData.email} onChange={handleInputChange} />
+                <input required type="password" name="email" value={formData.email} onChange={handleInputChange} />
             </label>
             <br />
             <label>
                 address:
-                <input type="text" name="street" value={formData.address.street} onChange={handleInputChange} />
-                <input type="text" name="suite" value={formData.address.suite} onChange={handleInputChange} />
-                <input type="text" name="city" value={formData.address.city} onChange={handleInputChange} />
-                <input type="text" name="zipcode" value={formData.address.zipcode} onChange={handleInputChange} />
+                <input required type="text" name="street" value={formData.address.street} placeholder='street' onChange={handleInputChange} />
+                <input required type="text" name="suite" value={formData.address.suite} placeholder='suite' onChange={handleInputChange} />
+                <input required type="text" name="city" value={formData.address.city} placeholder='city' onChange={handleInputChange} />
+                <input required type="text" name="zipcode" value={formData.address.zipcode} placeholder='zipcode' onChange={handleInputChange} />
             </label>
             <label>
             geo:
-            <input type="text" name="lat" value={formData.address.geo.lat} onChange={handleInputChange} />
-            <input type="text" name="lng" value={formData.address.geo.lng} onChange={handleInputChange} />
+            <input required type="text" name="lat" value={formData.address.geo.lat} placeholder='lat' onChange={handleInputChange} />
+            <input required type="text" name="lng" value={formData.address.geo.lng} placeholder='lng' onChange={handleInputChange} />
             </label>
             <br />
             <label>
             phone:
-                <input type="text" name="phone" value={formData.phone} onChange={handleInputChange} />
+                <input required type="text" name="phone" value={formData.phone} onChange={handleInputChange} />
             </label>
             <br />
             <label>
             website:
-                <input type="text" name="website" value={formData.website} onChange={handleInputChange} />
+                <input required type="text" name="website" value={formData.website} onChange={handleInputChange} />
             </label>
             <br />
             <label>
             company:
-            <input type="text" name="Name" value={formData.company.Name} onChange={handleInputChange} />
-            <input type="text" name="catchPhrase" value={formData.company.catchPhrase} onChange={handleInputChange} />
-            <input type="text" name="bs" value={formData.company.bs} onChange={handleInputChange} />
+            <input required type="text" name="Name" value={formData.company.Name} placeholder='Name' onChange={handleInputChange} />
+            <input required type="text" name="catchPhrase" value={formData.company.catchPhrase} placeholder='catchPhrase' onChange={handleInputChange} />
+            <input required type="text" name="bs" value={formData.company.bs} placeholder='bs' onChange={handleInputChange} />
             </label>
             <button type="submit">Sent</button>
         </form >
